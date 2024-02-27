1e0297d485a9954a2cd92c5668090add9b0e8085
Victor Dias Noleto <lzero1541@gmail.com>
2024-02-26 23:46:50 -0300

    ajustes as configuracoes do resize

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,5 @@
+import debounce from './debounce.js';
+
 export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
@@ -84,7 +86,7 @@ export default class Slide {
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
 
-    this.onResize = this.onResize.bind(this);
+    this.onResize = debounce(this.onResize.bind(this), 200);
   }
 
   // Slides config
