56ca557b228e9849a48e7386aa874b506bf4938c
Victor Dias Noleto <lzero1541@gmail.com>
2024-03-02 21:41:09 -0300

    finalizando o elemento de controle

--- js/slide.js
+++ js/slide.js
@@ -164,5 +164,12 @@ export class SlideNav extends Slide {
   createControl() {
     const control = document.createElement('ul');
     control.dataset.control = 'slide';
+
+    this.slideArray.forEach((item, index) => {
+      control.innerHTML += `<li><a href="#slide${index + 1}">${
+        index + 1
+      }</a></li>`;
+    });
+    this.wrapper.appendChild(control);
   }
 }
